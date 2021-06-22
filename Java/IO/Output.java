cf5277807808b99acb0f721709fb452014cee0cd
adminr00t <jcminniss@gmail.com>
2021-06-22 15:42:13 +1000

    Updated Java Folder
    part of relearning java

---
+++ Java/IO/Output.java
@@ -0,0 +1,7 @@
+package IO;
+public class Output {
+    public static void main(String[] args) { // int main (){}
+        System.out.println("Hello World!"); //cout << "Hello World!" << endl;
+    }
+    
+}
